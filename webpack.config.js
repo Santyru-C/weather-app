b4b30b44f5fd25c990d732f8b01960d40a9bda65
Santyru <santiago0988@hotmail.com>
2023-06-26 14:20:09 -0300

    add server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/DOMmanipulator.js',
   mode: 'development',
   devtool: 'inline-source-map',
   output: {
